65db0335067d31234ef2fa667e967bd11ffada2a
john maicon <68080301+john-maicon@users.noreply.github.com>
2024-02-07 02:49:09 +0000

    finaliza `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
 });
