b8a18551a15c584af2746570e9c4ab3d627c96f1
john maicon <68080301+john-maicon@users.noreply.github.com>
2024-02-08 11:10:53 +0000

    Adiconado log para capturar erro

--- infra/databases.js
+++ infra/databases.js
@@ -8,6 +8,13 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  console.log("credenciais do postgress", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
     await client.connect();
