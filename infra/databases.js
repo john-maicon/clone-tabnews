1b4bfb493a4dd46f61bfd2fb431a95a5ef18d5ce
john maicon <68080301+john-maicon@users.noreply.github.com>
2024-02-09 13:03:17 +0000

    database conexao ssl

--- infra/databases.js
+++ infra/databases.js
@@ -7,7 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
+
   console.log("credenciais do postgress", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
