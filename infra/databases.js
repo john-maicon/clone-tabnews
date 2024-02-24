bcab98a8de0a7010021403b2f21770ee757f701a
john maicon <68080301+john-maicon@users.noreply.github.com>
2024-02-24 21:44:54 +0000

    ajuste na config database

--- infra/databases.js
+++ infra/databases.js
@@ -27,7 +27,9 @@ export default {
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
-    return process.env.POSTGRES_CA;
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
   }
 
   return process.env.NODE_ENV === "development" ? false : true;
