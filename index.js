59df411a1156a6c3d3467821e156ba542a53e6d1
john <johnmaiconn@gmail.com>
2024-05-26 19:00:41 -0300

    feat: adiciona pagina inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+app.get('/teste', (req, res) => {
+    res.send('teste ');
+  });
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express rodando na porta ${PORT}`);
+});
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <h3>
+       hello word! {" "}
+      </h3>
+    );
+  }
+  
+  export default Home;
