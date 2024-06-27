4123692d0ac7f9abd0993d6a2ddc3d65526da975
john <johnmaiconn@gmail.com>
2024-06-26 21:21:34 -0300

    add `database.js` e `.env` files

--- pages/api/v1/status/index..js
+++ pages/api/v1/status/index..js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from "/infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 5;");
+  console.log(result.rows);
+
   res.status(200).json({ name: "John Doe" });
 }
 
