5fe46b9edfbef93c8082afa46ca098fd027fb116
john <johnmaiconn@gmail.com>
2024-10-26 10:58:08 -0300

    fix eslint linting

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -18,16 +18,3 @@ test('POST to /api/v1/migrations', async () => {
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
 });
-
-test('POST to /api/v1/migrations', async () => {
-  const response2 = await fetch('http://localhost:3000/api/v1/migrations', {
-    method: 'POST',
-  });
-
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
-});
