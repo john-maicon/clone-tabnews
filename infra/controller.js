a081e0c161b13829cdf96f0f4bddc02099c7b534
john maicon <johnmaiconn@gmail.com>
2025-05-01 17:31:35 -0300

    feat: create and use `NotFoundError` in controller.js

--- infra/controller.js
+++ infra/controller.js
@@ -1,4 +1,4 @@
-import { InternalServerError, MethodNotAllowedError, ValidationError } from 'infra/errors';
+import { InternalServerError, MethodNotAllowedError, ValidationError, NotFoundError } from 'infra/errors';
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -8,7 +8,7 @@ function onNoMatchHandler(request, response) {
 
 function onErrorHandler(error, request, response) {
 
-  if (error instanceof ValidationError) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
     return response.status(error.statusCode).json(error);
   }
 
--- infra/errors.js
+++ infra/errors.js
@@ -61,6 +61,26 @@ export class ValidationError extends Error {
   }
 }
 
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || 'Não foi possivel encontrar o recurso no sistema', {
+      cause,
+    });
+    this.name = 'NotFoundError';
+    this.action = action ||'Verifique se os parametros estão corretos';
+    this.statusCode = 404;
+  }
+
+  // para deixar as props como enumeraveis.
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
 
 export class MethodNotAllowedError extends Error {
   constructor() {
